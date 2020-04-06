41d7bb338fff0d381233681faf85c976e14c1a0f
gattidanielmarco <gatti.daniel.marco@iisgalvanimi.edu.it>
2020-04-06 13:19:52 +0000

    1.0 fatto

--- first-routed-app/src/app/spotify.service.ts
+++ first-routed-app/src/app/spotify.service.ts
@@ -11,7 +11,7 @@ export class SpotifyService {
     const url = `https://api.spotify.com/v1/search?q=${query}&type=track`;
     const headers = new HttpHeaders({
       Authorization:
-        'Bearer BQBBixbe8Z8MOV6X1CD45caEuZaFTeXj7WnuhWLGTKxjxtcGvvSf9PdB7F0XRnoY5PANs7-xY-L2AZC6PCNp7y19Klrqg6T9yaYgtOvCgwJFsHXteca7BNeYbtaEXWvWTTQ8_SOiTrvXjys0pN9VJwrmnCnr9qY'
+        'Bearer BQC-d0gf_kgXoGm1X2BFFBErpViFJBaJef4wPYlD4aMUpDmmnygxVclIopIC99u5CJIeOPXLYw8Jsgg_ThHgXgMMqvyoR2vaxX5aF3aZMgT3McWOWfk-2HailU4pYovc5SgtSIK6RcxrHh44rrkesoPmRNEihK8'
     });
 
     let obsTracks = this.http.get(url, { headers });
