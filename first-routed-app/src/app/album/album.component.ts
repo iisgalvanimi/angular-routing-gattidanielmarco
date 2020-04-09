ca487f03af80189fe34e8682633524305a0a22f6
gattidanielmarco <gatti.daniel.marco@iisgalvanimi.edu.it>
2020-04-09 10:09:16 +0000

    non funziona

--- first-routed-app/src/app/album/album.component.ts
+++ first-routed-app/src/app/album/album.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { SpotifyService } from '../spotify.service';
 
 @Component({
   selector: 'app-album',
@@ -6,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./album.component.css']
 })
 export class AlbumComponent implements OnInit {
+  query: string;
+  obsTrack: Observable<Object>;
+  results: any;
+  constructor(public spotify: SpotifyService) { }
+  submit(query: HTMLInputElement): void {
 
-  constructor() { }
-
+    if (!query.value) {
+      return;
+    }
+    this.query = query.value;
+    this.obsTrack = this.spotify.getAlbum(this.query);
+    this.obsTrack.subscribe((data) => { this.results = data; console.log(this.results) });
+  }
+  renderResults(res: any): void {
+    this.results = null;
+    if (res && res.album && res.album.items) {
+      this.results = res.album.items;
+    }
+  }
   ngOnInit(): void {
   }
 
