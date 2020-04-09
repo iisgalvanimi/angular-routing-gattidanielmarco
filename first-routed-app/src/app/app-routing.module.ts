320c2368b95c0ba314caa11850c0a94273282a97
gattidanielmarco <gatti.daniel.marco@iisgalvanimi.edu.it>
2020-04-09 09:24:08 +0000

    punto 5

--- first-routed-app/src/app/app-routing.module.ts
+++ first-routed-app/src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { AboutComponent } from './about/about.component';
 import { SearchComponent } from './search/search.component';
 import { TrackComponent } from './track/track.component';
+import { AlbumComponent } from './album/album.component';
 
 const routes: Routes = [
   { path: 'about', component: AboutComponent },
   { path: 'search', component: SearchComponent },
   { path: 'tracks/:id', component: TrackComponent },
-  { path: '',   redirectTo: '/search', pathMatch: 'full' }
+  { path: '',   redirectTo: '/search', pathMatch: 'full' },
+  { path: 'album/:id', component: AlbumComponent }
 ];
 
 @NgModule({
